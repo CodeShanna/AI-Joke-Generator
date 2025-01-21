f6b9e327f26b10d7184c0cfcdf5173d40a0a6220
Shanna Jones <shanna@jonesbunch.com>
2025-01-21 13:52:11 +0100

    updated comments for myself

--- src/index.js
+++ src/index.js
@@ -12,17 +12,18 @@ function displayJoke(response) {
 }
 
 function createJoke (event) {
-    event.preventDefault();
-let apiKey = "bfaafcoc80td400cfa803cbf6c5ff69f";
-let context =
-"you are a hilarious comedian who is a 48 year old woman and you have been suffering from menopause symptoms. You want to make people laugh by telling jokes about getting older and body changes. Please leave an empty line between jokes. Please tell one joke at a time.";
-let prompt = "Only say 1 joke at a time.  Please use the format of (question, why did the woman cry all night?) (answer, she just said goodbye to her youth.)";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  event.preventDefault();
+  let apiKey = "bfaafcoc80td400cfa803cbf6c5ff69f";
+  let context =
+    "you are a hilarious comedian who is a 48 year old woman and you have been suffering from menopause symptoms. You want to make people laugh by telling jokes about getting older and body changes. Please leave an empty line between jokes. Please tell one joke at a time.";
+  let prompt =
+    "Only say 1 joke at a time.  Please use the format of (question, why did the woman cry all night?) (answer, she just said goodbye to her youth.)";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-let joke = document.querySelector("#joke");
-    joke.innerHTML = "Joke incoming!!!";
-
-axios.get(apiUrl).then(displayJoke);
+  let joke = document.querySelector("#joke");
+  joke.innerHTML = "Joke incoming!!!";
+  //placed the axios response inside the function so it does not automatically run. We want the button click to request the joke.
+  axios.get(apiUrl).then(displayJoke);
 }
 
 const jokeElement = document.getElementById(`button`);
